312e8baa4ef0b6ef88bdf7754e450a8a676c6d21
HanumanthaRao MVL <hanumanth@flux7.com>
2019-07-09 23:52:07 +0530

    Flux7 custom git pre-commit hooks

---
+++ pre-commit-hooks/check-readme.py
@@ -0,0 +1,13 @@
+import os, fnmatch
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name.lower(), pattern.lower()):
+                result.append(os.path.join(root, name))
+    print result
+    return result
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+find('README.MD', dir_path)
+
