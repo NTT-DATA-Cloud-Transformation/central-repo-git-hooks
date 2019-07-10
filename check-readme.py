dc812c9482860dd2e4cdd6f5c20e2500245d314b
HanumanthaRao MVL <hanumanth@flux7.com>
2019-07-10 17:01:55 +0530

    config updates

--- check-readme.py
+++ check-readme.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
-import os, fnmatch
+import os, sys, fnmatch
 def find(pattern, path):
     result = []
     for root, dirs, files in os.walk(path):
         for name in files:
             if fnmatch.fnmatch(name.lower(), pattern.lower()):
                 result.append(os.path.join(root, name))
-    print result
-    return result
+    return len(result)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-find('README.MD', dir_path)
+count = find('README.MD', dir_path)
+
+if not count >=1:
+    print("ERROR: Readme file not found. Please add readme file and try again !!")
+    sys.exit(1)
 
