871a64495153bf561ff6f0e57bd1684821128690
Hanumanth <hanumanth@flux7.com>
2020-06-19 18:39:26 +0000

    adding response_code validation

--- developer-hooks.sh
+++ developer-hooks.sh
@@ -3,13 +3,20 @@
 ## Pre-commit hook validation script for the developers.
 
 ## Variables
-HOOK_FILE = "https://github.com/Flux7Labs/central-repo-git-hooks/blob/master/.pre-commit-config.yaml"
-ARGS = "--all-files"
-PKGS = " pre-commit cfn-lint flake8"
+HOOK_FILE="https://raw.githubusercontent.com/Flux7Labs/central-repo-git-hooks/blob/master/.pre-commit-config.yaml"
+ARGS="--all-files"
+PKGS=" pre-commit cfn-lint flake8"
 
-## Pre-commit install and validation
-pre-commit install ${PKGS}
-pre-commit clean
-pre-commit run -c ${HOOK_FILE} ${ARGS}
-pre-commit clean
+## Validate the HOOK_FILE ResponseCode.
+RESP_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" ${HOOK_FILE})
 
+if ${RESP_CODE} == 200; then
+    ## Pre-commit install and validation
+    pip install ${PKGS}
+    pre-commit install
+    pre-commit clean
+    pre-commit run -c ${HOOK_FILE} ${ARGS}
+    pre-commit clean
+else
+    echo "Unable to download ${HOOK_FILE}. Exiting.."
+fi
