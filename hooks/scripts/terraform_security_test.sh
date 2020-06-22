f71dcf16da715aa3753084851ab811aa8c8f9c42
vahram@exammed.com <vahram@flux7.com>
2020-06-22 18:57:09 -0300

    Testing

--- hooks/scripts/terraform_security_test.sh
+++ hooks/scripts/terraform_security_test.sh
@@ -5,7 +5,9 @@ set -e
 # especially as Apple changes the GUI app behavior every release (see https://stackoverflow.com/q/135688/483528). As a
 # workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
 export PATH=$PATH:/usr/local/bin
+echo "Starting checkov using"
 echo "$@"
 for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
+  echo $dir
   checkov -d $dir
 done
