9f561d8fc24be3a01ac439d18fa85d1c054e5527
Hanumanth <hanumanth@flux7.com>
2020-07-06 23:31:14 +0530

    adding new hook script for pr template check

---
+++ hooks/scripts/check-prtemplate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Check for readme.md files in the repository.
+
+count=`find . -type f -iname 'pull_request_template.md' | wc -l`
+
+if [[ ! $count -ge 1 ]]; then
+    echo "ERROR: PR Template found in the repository. Please create one and try again !!"
+    echo "Standard Location: .github/pull_request_template.md"
+    exit 1
+else
+    exit 0
+fi
+
