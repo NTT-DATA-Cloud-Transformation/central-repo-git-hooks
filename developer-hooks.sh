188f1655e1b093eb41cf800f26110d8782e196ae
Prashant Tiwari <prashant@flux7.com>
2020-08-25 20:59:08 +0530

    Added temp location for storing hook file and made changes in the if condition

--- developer-hooks.sh
+++ developer-hooks.sh
@@ -2,21 +2,28 @@
 
 ## Pre-commit hook validation script for the developers.
 
+
 ## Variables
-HOOK_FILE="https://raw.githubusercontent.com/Flux7Labs/central-repo-git-hooks/master/.pre-commit-config.yaml"
+HOOK_FILE_URL="https://raw.githubusercontent.com/Flux7Labs/central-repo-git-hooks/master/.pre-commit-config.yaml"
 ARGS="--all-files"
 PKGS="pre-commit cfn-lint flake8 checkov"
+BASEDIR="/tmp/hooks"
+HOOK_FILE="${BASEDIR}/hookfile"
 
-## Validate the HOOK_FILE ResponseCode.
-RESP_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" ${HOOK_FILE})
+# create directory to store hook file temporarily
+mkdir -p ${BASEDIR}
 
-if ${RESP_CODE} == 200; then
-    ## Pre-commit install and validation
-    pip install ${PKGS}
+# cURL to fetch the hook file
+curl ${HOOK_FILE_URL} > ${HOOK_FILE}
+
+
+if [[ -f ${HOOK_FILE} ]]; then
+    pip3 install $PKGS
     pre-commit install
     pre-commit clean
     pre-commit run -c ${HOOK_FILE} ${ARGS}
     pre-commit clean
+    rm -f ${HOOK_FILE}
 else
-    echo "Unable to download ${HOOK_FILE}. Exiting.."
+    echo "Unable to download ${HOOK_FILE_URL}. Exiting.."
 fi
