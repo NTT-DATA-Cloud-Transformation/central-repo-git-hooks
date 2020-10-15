8afe176feb9c565ecd11d9ed4e2d37019fc464d7
Kritika Agarwal <kritika@flux7.com>
2020-10-15 20:51:35 +0530

    using regex for print statement was getting used for logging

--- hooks/scripts/check-print-keyword.sh
+++ hooks/scripts/check-print-keyword.sh
@@ -2,7 +2,7 @@
 
 ## Check for print keyword used for logging in the repository.
 
-count=`grep -r "print" . | wc -l`
+count=`grep -r 'print\s*(' . | wc -l`
 
 if [[ ! $count -ge 1 ]]; then
     echo "ERROR: print statement found in the repository. Please use custom_logger or python logging module and try again !!"
